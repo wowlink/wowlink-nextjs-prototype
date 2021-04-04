5a0d14fa5ba12c0ac678afe470ece4d2f4196f79
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-04 08:08:38 -0700

    feat: add fake lookup and conversion

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,7 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-// http://localhost:3000/api/redirect?repo=test&wow=abc
+// Use http://localhost:3000/api/redirect?repo=test&wow=gh for testing to navigate to GitHub
 export default (req: NextApiRequest, res: NextApiResponse) => {
-    console.log(req.query)
-    res.redirect('https://github.com')
+    const wowlink: string = req.query['wow'] as string;
+    console.log(wowlink);
+    const lookup: Record<string, string> = {
+        "gh": "https://github.com",
+        "goo": "https://google.com",
+    };
+    // This should be a function that looks up wowlink existence
+    if (wowlink in lookup) {
+        // The link conversion should also be some kind of API
+        // to make future implementation of partial replacement
+        // easier.
+        res.redirect(lookup[wowlink]);
+    } else {
+        res.redirect('/');
+    }
 }
