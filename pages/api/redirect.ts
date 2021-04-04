1d363fbcfba415902a14a81ca409bd7bf8b0aaec
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-04 08:40:53 -0700

    feat: add gh file fetching

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-// import { Octokit } from "@octokit/rest";
+import { Octokit } from "@octokit/rest";
 
 // Use http://localhost:3000/api/redirect?repo=test&wow=gh for testing to navigate to GitHub
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     const wowlink: string = req.query['wow'] as string;
     console.log(wowlink);
+    // This should be reading from the source
+    const octokit = new Octokit();
+    const configResponse = await octokit.rest.repos.getContent({
+        owner: "wowlink",
+        repo: "default-profile",
+        path: "config.yaml",
+    });
+    console.log(Buffer.from(configResponse.data['content'], 'base64').toString());
     const lookup: Record<string, string> = {
         "gh": "https://github.com",
         "goo": "https://google.com",
