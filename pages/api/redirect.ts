ea8994c8d41caac6943f893145794d2237d752c9
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-18 01:37:56 -0700

    feat: switch to using fetcher package

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,27 +1,18 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { Octokit } from "@octokit/rest";
-import yaml from "js-yaml";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { WowLookupFetcher, BuiltInLookupFetcherType, WowLookupFetchRequest, WowLookupFetcherConfig, WowLookupFetchResponse } from "wow-interface";
+import { WowLookupFetcherFactory } from "wow-lookup-fetcher";
 
 // Use http://localhost:3000/api/redirect?repo=test&wow=gh for testing to navigate to GitHub
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const wowlink: string = req.query['wow'] as string;
-    console.log(wowlink);
-    // This should be reading from the source
-    // 1. github repository
-    // 2. gist
-    // 3. local file
-    // 4. requesting custom endpoint
-    const octokit = new Octokit();
-    const configResponse = await octokit.rest.repos.getContent({
-        owner: "wowlink",
-        repo: "default-profile",
-        path: "config.yaml",
-    });
-    const configStr = Buffer.from(configResponse.data['content'], 'base64').toString();
-    console.log(configStr);
-    const config = yaml.load(configStr);
-    console.log(config);
-    const lookup: Record<string, string> = config['wowlinks'];
+    const fetcher_config: WowLookupFetcherConfig = {
+        githubUser: "wowlink",
+        githubRepository: "default-profile"
+    };
+    const fetcher: WowLookupFetcher = WowLookupFetcherFactory(BuiltInLookupFetcherType.GitHub, fetcher_config);
+    const fetch_req: WowLookupFetchRequest = {};
+    const fetch_res: WowLookupFetchResponse = await fetcher.fetch(fetch_req);
+    const lookup: Record<string, string> = fetch_res.wowMapping;
     // This should be a function that looks up wowlink existence
     if (wowlink in lookup) {
         // The link conversion should also be some kind of API
@@ -31,4 +22,4 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     } else {
         res.redirect('/');
     }
-}
+};
