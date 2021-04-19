55e76005c880a09f9fead27621e0893c978972e7
Tianhao Zhou <tianhaoz@umich.edu>
2021-04-18 17:05:25 -0700

    feat: move to use converter

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { WowLookupFetcher, BuiltInLookupFetcherType, WowLookupFetchRequest, WowLookupFetcherConfig, WowLookupFetchResponse } from "wow-interface";
+import {
+    WowLookupFetcher,
+    BuiltInLookupFetcherType,
+    WowLookupFetchRequest,
+    WowLookupFetcherConfig,
+    WowLookupFetchResponse,
+    BuiltInWowUrlConverterType,
+    WowUrlConverter,
+    WowUrlConverterConfig,
+    WowUrlConvertRequest,
+    WowUrlConvertResponse,
+} from "wow-interface";
 import { WowLookupFetcherFactory } from "wow-lookup-fetcher";
+import { WowUrlConverterFactory } from "wow-url-converter";
 
 // Use http://localhost:3000/api/redirect?repo=test&wow=gh for testing to navigate to GitHub
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -9,17 +21,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         githubUser: "wowlink",
         githubRepository: "default-profile"
     };
-    const fetcher: WowLookupFetcher = WowLookupFetcherFactory(BuiltInLookupFetcherType.GitHub, fetcher_config);
+    const fetcher: WowLookupFetcher = WowLookupFetcherFactory(
+        BuiltInLookupFetcherType.GitHub, fetcher_config);
     const fetch_req: WowLookupFetchRequest = {};
     const fetch_res: WowLookupFetchResponse = await fetcher.fetch(fetch_req);
-    const lookup: Record<string, string> = fetch_res.wowMapping;
-    // This should be a function that looks up wowlink existence
-    if (wowlink in lookup) {
-        // The link conversion should also be some kind of API
-        // to make future implementation of partial replacement
-        // easier.
-        res.redirect(lookup[wowlink]);
-    } else {
-        res.redirect('/');
-    }
+    const converter_config: WowUrlConverterConfig = {
+        fetcherResponse: fetch_res
+    };
+    const converter: WowUrlConverter = WowUrlConverterFactory(
+        BuiltInWowUrlConverterType.Basic, converter_config);
+    const converter_req: WowUrlConvertRequest = {
+        wowUrl: wowlink,
+    };
+    const converter_res: WowUrlConvertResponse = converter.convert(converter_req);
+    res.redirect(converter_res.fullUrl);
 };
